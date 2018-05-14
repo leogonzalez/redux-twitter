e684ed33a4e0c3da085dfd59d6f7a3c83d8aa2fd
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-05-14 19:36:53 -0300

    added logger middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
+import logger from './logger'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default composeEnhancers(applyMiddleware(ReduxThunk));
+export default composeEnhancers(applyMiddleware(ReduxThunk, logger));
