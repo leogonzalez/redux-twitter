01f22eb69c90d1efc2214d306e3b36bc05db9b81
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-05-20 18:40:57 -0300

    heroku prod config

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 if (process.env.NODE_ENV === "production") {
   // DEBUG: PLEASE CHANGE THIS WHEN WE HAVE A PROD ENVIRONMENT
+  console.log('PRODUCTION ON');
   module.exports = require("./dev");
 } else {
   module.exports = require("./dev");
