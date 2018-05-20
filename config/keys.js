b91d9d3ae3faac6a75ef137cc137bce68b74b129
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-05-20 18:44:51 -0300

    prod env vars

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   // DEBUG: PLEASE CHANGE THIS WHEN WE HAVE A PROD ENVIRONMENT
   console.log('PRODUCTION ON');
-  module.exports = require("./dev");
+  module.exports = require("./prod");
 } else {
   module.exports = require("./dev");
 }
