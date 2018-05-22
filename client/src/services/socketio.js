54648f33be4ea30808ed5df6b67dad32eef516a1
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-05-21 21:02:11 -0300

    production config

--- client/src/services/socketio.js
+++ client/src/services/socketio.js
@@ -2,7 +2,11 @@ import socketIOClient from "socket.io-client";
 import { receiveTweets } from "../actions/tweets";
 
 export const ioDispatch = dispatch => {
-  const socket = socketIOClient("http://localhost:3001");
+  let port = "http://localhost:3001";
+  if (process.env.NODE_ENV === "production") {
+    port = "/";
+  }
+  const socket = socketIOClient(port);
   socket.on("newTweet", doc => {
     console.log("New Tweet received from server: ", doc);
     dispatch(receiveTweets({ [doc._id]: doc }));
